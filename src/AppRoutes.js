1561df32d484818e0299c7378e71950c679c868e
Icarus <fodab1448@gmail.com>
2024-11-24 17:00:28 -0300

    Página de login como padrão

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -17,7 +17,7 @@ const Stack = createStackNavigator();
 const AppRoutes = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Feed">
+            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="Register" component={RegisterScreen} />                
                 <Stack.Screen name="Explore" component={Explore} />                
